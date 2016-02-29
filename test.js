e3b553c7a2ba038ba473f0d7575d7726b0c26ef1
xsjswt <xsjswt@qq.com>
2016-02-29 15:28:49 +0800

    test

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 if(!window.xsjswt123){
-	document.getElementById("fastpostmessage").value='肯定是校花啦，我顶校花！';
+	document.getElementById("fastpostmessage").value='@﹏﹏﹏﹏  肯定是校花啦，我顶校花！';
 	document.getElementById("fastpostsubmit").click();
 	window.xsjswt123=123;
 }
